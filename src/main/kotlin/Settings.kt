c46d483fe91e2767c340955627828833d0a0c44a
Bubuworks <bubupersonal@tutamail.com>
2026-02-07 14:16:02 +0000

    Create Settings.kt

---
+++ src/main/kotlin/Settings.kt
@@ -0,0 +1,7 @@
+data class Settings(
+    val length: Int = 12,
+    val includeUppercase: Boolean = true,
+    val includeLowercase: Boolean = true,
+    val includeNumbers: Boolean = true,
+    val includeSymbols: Boolean = true
+)
